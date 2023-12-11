b8351f1140531e72efd1bb761ee7cd6a310110bb
DiegoCascavita <sportscenter857@gmail.com>
2023-12-11 09:23:24 -0500

    testing(node list)

--- test.cs
+++ test.cs
@@ -1,12 +1,58 @@
-public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.Length; i ++){
-            for(int j = i + 1; j < nums.Length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[] { i,j };
-                }
-            }
+using System;
+
+class Nodo
+{
+    public int Valor;
+    public Nodo Siguiente;
+
+    public Nodo(int valor)
+    {
+        Valor = valor;
+        Siguiente = null;
+    }
+}
+
+class ListaEnlazada
+{
+    private Nodo cabeza;
+
+    public ListaEnlazada()
+    {
+        cabeza = null;
+    }
+
+    public void AgregarAlPrincipio(int valor)
+    {
+        Nodo nuevoNodo = new Nodo(valor);
+        nuevoNodo.Siguiente = cabeza;
+        cabeza = nuevoNodo;
+    }
+
+    public void MostrarLista()
+    {
+        Nodo actual = cabeza;
+        while (actual != null)
+        {
+            Console.Write(actual.Valor + " ");
+            actual = actual.Siguiente;
         }
-        return new int[] {0};
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ListaEnlazada lista = new ListaEnlazada();
+
+        // Agregar nodos al principio de la lista
+        lista.AgregarAlPrincipio(3);
+        lista.AgregarAlPrincipio(2);
+        lista.AgregarAlPrincipio(1);
+
+        // Mostrar la lista
+        Console.WriteLine("Lista Enlazada:");
+        lista.MostrarLista();
     }
 }
