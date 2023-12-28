63d6a4c226ec5917817f671dacd0de6d41d18b62
DiegoCascavita <sportscenter857@gmail.com>
2023-12-28 08:21:27 -0500

    trees in c#

--- test.cs
+++ test.cs
@@ -1,32 +1,52 @@
 using System;
 using System.Collections.Generic;
 
-class Program
+// Define a basic TreeNode class
+public class TreeNode
 {
-    static void Main()
+    public int Value { get; }
+    public List<TreeNode> Children { get; }
+
+    public TreeNode(int value)
     {
-        // Create a stack of integers
-        Stack<int> miStack = new Stack<int>();
+        Value = value;
+        Children = new List<TreeNode>();
+    }
 
-        // Push elements onto the stack
-        miStack.Push(3);
-        miStack.Push(7);
-        miStack.Push(1);
-        miStack.Push(4);
-
-        // Display the elements in the stack
-        Console.WriteLine("Elements in the stack:");
-        foreach (int elemento in miStack)
-        {
-            Console.WriteLine(elemento);
-        }
-
-        // Pop elements from the stack
-        Console.WriteLine("\nPopping elements from the stack:");
-        while (miStack.Count > 0)
-        {
-            int elemento = miStack.Pop();
-            Console.WriteLine(elemento);
-        }
+    public void AddChild(TreeNode childNode)
+    {
+        Children.Add(childNode);
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating nodes for the tree
+        TreeNode rootNode = new TreeNode(1);
+
+        TreeNode childNode1 = new TreeNode(2);
+        TreeNode childNode2 = new TreeNode(3);
+        TreeNode childNode3 = new TreeNode(4);
+
+        TreeNode grandChildNode1 = new TreeNode(5);
+        TreeNode grandChildNode2 = new TreeNode(6);
+
+        // Building the tree structure
+        rootNode.AddChild(childNode1);
+        rootNode.AddChild(childNode2);
+        rootNode.AddChild(childNode3);
+
+        childNode1.AddChild(grandChildNode1);
+        childNode1.AddChild(grandChildNode2);
+
+        // Accessing the tree
+        Console.WriteLine($"Root Node Value: {rootNode.Value}");
+        Console.WriteLine($"Number of Children of Root Node: {rootNode.Children.Count}");
+
+        Console.WriteLine($"Child Node 1 Value: {childNode1.Value}");
+        Console.WriteLine($"Number of Children of Child Node 1: {childNode1.Children.Count}");
+
+        Console.WriteLine($"Grandchild Node 1 Value: {grandChildNode1.Value}");
+        Console.Writ
