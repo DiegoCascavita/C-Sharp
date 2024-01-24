c185d925b288f95b2f8b8147ba2abd667aea19f9
DiegoCascavita <sportscenter857@gmail.com>
2024-01-24 09:52:55 -0500

    update(modules)

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947c56f423f260b9f9741a6185d6054ffae9d0d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0786afebdfeb4ea6ef24a4632a2d5db5d11b519e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
