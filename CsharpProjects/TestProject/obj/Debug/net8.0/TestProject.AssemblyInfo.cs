daf5981dbfb0f80dffdf287c89e9c09ce8ae016f
DiegoCascavita <sportscenter857@gmail.com>
2024-01-29 12:49:34 -0500

    [hashmap] c#

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0786afebdfeb4ea6ef24a4632a2d5db5d11b519e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c185d925b288f95b2f8b8147ba2abd667aea19f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- test.cs
+++ test.cs
@@ -1,45 +1,43 @@
-class Persona 
-{
-    private string nombre;
-    private int edad;
+using System;
+using System.Collections.Generic;
 
-    //Propiedades Públicas para acceder alos campos privados
-    public string Nombre
-    {
-        get {return nombre;}
-        set {nombre = value;}
-    }
-
-    public int Edad
-    {
-        get {return edad;}
-        set
-        {
-            if(value >= 0)
-            {
-                edad = value;
-            }
-            else
-            {
-                Console.WriteLine("La edad no puede ser un valor negativo.")
-            }
-        }
-    }
-    public void MostrarInformacion()
-    {
-        Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
-    }
-}
-//---------------------------------------------------------------
 class Program
 {
     static void Main()
     {
-        Persona persona = new Persona();
+        // Creating a dictionary with string keys and int values
+        Dictionary<string, int> myDictionary = new Dictionary<string, int>();
 
-        persona.Nombre = "Juan";
-        persona.Edad = 25;
+        // Adding key-value pairs
+        myDictionary.Add("One", 1);
+        myDictionary.Add("Two", 2);
+        myDictionary.Add("Three", 3);
 
-        persona.MostrarInformacion();
+        // Accessing values by keys
+        Console.WriteLine("The value of key 'Two' is: " + myDictionary["Two"]);
+
+        // Modifying values
+        myDictionary["Three"] = 30;
+
+        // Iterating over key-value pairs
+        foreach (var pair in myDictionary)
+        {
+            Console.WriteLine("Key: " + pair.Key + ", Value: " + pair.Value);
+        }
+
+        // Checking if a key exists
+        if (myDictionary.ContainsKey("Four"))
+        {
+            Console.WriteLine("Key 'Four' exists.");
+        }
+        else
+        {
+            Console.WriteLine("Key 'Four' does not exist.");
+        }
+
+        // Removing a key-value pair
+        myDictionary.Remove("Two");
+
+        Console.ReadLine();
     }
 }
