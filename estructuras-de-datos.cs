ff4b74e7c21ca507a1318cde819c2cc10e233aec
DiegoCascavita <sportscenter857@gmail.com>
2024-02-02 08:24:22 -0500

    [DSA] hashmap / dictionary

--- estructuras-de-datos.cs
+++ estructuras-de-datos.cs
@@ -48,3 +48,48 @@ string elemento = cola.Dequeue(); // elemento contiene "Elemento 1"
    
 
 int 
+
+// -------------------HASHMAP -------------------
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Creating a dictionary with string keys and int values
+        Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+        // Adding key-value pairs
+        myDictionary.Add("One", 1);
+        myDictionary.Add("Two", 2);
+        myDictionary.Add("Three", 3);
+
+        // Accessing values by keys
+        Console.WriteLine("The value of key 'Two' is: " + myDictionary["Two"]);
+
+        // Modifying values
+        myDictionary["Three"] = 30;
+
+        // Iterating over key-value pairs
+        foreach (var pair in myDictionary)
+        {
+            Console.WriteLine("Key: " + pair.Key + ", Value: " + pair.Value);
+        }
+
+        // Checking if a key exists
+        if (myDictionary.ContainsKey("Four"))
+        {
+            Console.WriteLine("Key 'Four' exists.");
+        }
+        else
+        {
+            Console.WriteLine("Key 'Four' does not exist.");
+        }
+
+        // Removing a key-value pair
+        myDictionary.Remove("Two");
+
+        Console.ReadLine();
+    }
+}
