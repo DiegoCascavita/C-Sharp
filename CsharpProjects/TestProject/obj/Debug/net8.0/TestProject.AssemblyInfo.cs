39e21d6a8b04b5d2af8612260be66ff9c4d82432
Diego C <sportscenter857@gmail.com>
2024-05-22 12:59:18 -0500

    [Assembly] dependencies

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+506501de14e7f97d27f3ffe97e8fadac5b235448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8358aa545143275e90678b366e8840b0360fcd6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
