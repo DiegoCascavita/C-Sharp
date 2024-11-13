1cc77e1b89225ad0136f06d7588f1ffd74e65b3c
Diego C <sportscenter857@gmail.com>
2024-11-13 11:58:14 -0500

    assembly

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c65513220646c4a68abbb0f4c833b3aa78c173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f27848ec3241209eb24fd5f3011387037e1f3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
